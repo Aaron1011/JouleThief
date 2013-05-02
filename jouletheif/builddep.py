e7845898b3f3eac4b674abeeef13d469359fe9de
Louis Goessling <louis@goessling.com>
2013-05-02 13:32:58 -0500

    Build script!

--- jouletheif/builddep.py
+++ jouletheif/builddep.py
@@ -1,4 +1,12 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['joulethief.py'])
+setup(console=['joulethief.py'],
+      options={
+                "py2exe":{
+                        "bundle_files":1,#1,
+                        "includes":["pygame"],
+                        "ascii":False,
+                        "optimize":2
+                }
+        })
--- jouletheif/buildrun.py
+++ jouletheif/buildrun.py
@@ -1,4 +1,13 @@
-import os, shutil
+import os, shutil, zipfile, glob
+folder = 'dist'
+for the_file in os.listdir(folder):
+    file_path = os.path.join(folder, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+    except Exception, e:
+        print e
+#shutil.rmtree("dist")
 os.system("python builddep.py py2exe")
 try:
     shutil.copytree("lvl/", "dist/lvl/")
@@ -9,4 +18,17 @@ except Exception: print "Config already copied!"
 try:
     shutil.copytree("image/", "dist/image/")
 except Exception: print "Images already copied!"
-shutil.rmtree('dist')
+shutil.rmtree('build')
+shutil.rmtree('dist/tcl')
+shutil.copy("C:\Python27\lib\site-packages\pygame\SDL_ttf.dll", "dist/SDL.dll")
+shutil.copy("C:\Python27\lib\site-packages\pygame\libogg-0.dll", "dist/libogg-0.dll")
+print "Zipping..."
+os.system("del buildto/build.zip")
+myzip=zipfile.ZipFile('buildto/build.zip', 'w')
+for i in glob.glob("dist/*.*"):
+    myzip.write(i)
+for i in glob.glob("dist/lvl/*.*"):
+    myzip.write(i)
+for i in glob.glob("dist/image/*.*"):
+    myzip.write(i)
+myzip.close()
